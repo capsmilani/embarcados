0a98824e5114ab5d73ef6560f77a74e9787d18d5
capsmilani <70613756+capsmilani@users.noreply.github.com>
2020-12-17 18:00:48 -0300

    Add files via upload

---
+++ Lab07/I2C/Sources/Events.c
@@ -0,0 +1,82 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : I2C
+**     Processor   : MCF52259CAG80
+**     Component   : Events
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2017-06-13, 17:09, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnCoreWatchdogINT - void Cpu_OnCoreWatchdogINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+extern char flag;
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnCoreWatchdogINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TI1_OnInterrupt(void){
+  /* Write your code here ... */
+	flag--;
+}
+
+/* END Events */
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
---
+++ Lab07/I2C/Sources/Events.h
@@ -0,0 +1,83 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : I2C
+**     Processor   : MCF52259CAG80
+**     Component   : Events
+**     Compiler    : CodeWarrior MCF C Compiler
+**     Date/Time   : 2017-06-13, 17:09, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnCoreWatchdogINT - void Cpu_OnCoreWatchdogINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "I2C1.h"
+#include "TI1.h"
+
+void Cpu_OnCoreWatchdogINT(void);
+/*
+** ===================================================================
+**     Event       :  Cpu_OnCoreWatchdogINT (module Events)
+**
+**     Component   :  Cpu [MCF52259_144_LQFP]
+**     Description :
+**         This event is called when the OnCoreWatchdog interrupt had
+**         occurred. This event is automatically enabled when the <Mode>
+**         is set to 'Interrupt'.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+
+void TI1_OnInterrupt(void);
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale MCF series of microcontrollers.
+**
+** ###################################################################
+*/
